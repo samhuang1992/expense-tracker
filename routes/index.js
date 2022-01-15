fbb6d9f73a20c76ce9f91d7bff656a8c7cdf0147
samhuang1992 <bob60117@gmail.com>
2022-01-15 15:05:26 +0800

    feat: 新增home router

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+// 呼叫express提供的路由器
+const router = express.Router()
+
+const home = require('./modules/home')
+const record = require('./modules/record')
+const user = require('./modules/user')
+
+router.use('/', home)
+router.use('/record', record)
+// router.use('/user', user)
+
+// 匯出路由
+module.exports = router
