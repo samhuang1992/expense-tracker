506f7b2b84b08e7767c793de2394668280c8d16b
samhuang1992 <bob60117@gmail.com>
2022-01-16 22:39:23 +0800

    feat: 新增登入&註冊頁面

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,11 @@ const router = express.Router()
 
 const home = require('./modules/home')
 const record = require('./modules/record')
-const user = require('./modules/user')
+const users = require('./modules/users')
 
 router.use('/', home)
 router.use('/record', record)
-router.use('/user', user)
+router.use('/users', users)
 
 // 匯出路由
 module.exports = router
